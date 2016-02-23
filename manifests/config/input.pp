535a5a96ffd8068f6f2bf046d5552b8c9e8fbcc6
Joseph Larionov <jlariono@willamette.edu>
2016-02-23 12:26:33 -0800

    Added support for specifying event log query to input define

--- manifests/config/input.pp
+++ manifests/config/input.pp
@@ -2,7 +2,7 @@
 #
 # Example Puppet Code:
 # ::nxlog::config::input { 'sawyer':
-#   output_module  => 'im_msvistalog',
+#   input_module   => 'im_msvistalog',
 #   input_execs    => [
 #     'delete($Keywords)',
 #     '$raw_event = to_json()',
@@ -12,12 +12,13 @@
 # Resulting output:
 #
 define nxlog::config::input (
-  $conf_dir     = $::nxlog::conf_dir,
-  $conf_file    = $::nxlog::conf_file,
-  $input_execs  = $::nxlog::input_execs,
+  $conf_dir        = $::nxlog::conf_dir,
+  $conf_file       = $::nxlog::conf_file,
+  $input_execs     = $::nxlog::input_execs,
   $input_file_path = $::nxlog::input_file_path,
-  $input_module = $::nxlog::input_module,
-  $order_input  = $::nxlog::order_input,) {
+  $input_module    = $::nxlog::input_module,
+  $input_query     = $::nxlog::input_query,
+  $order_input     = $::nxlog::order_input,) {
   concat::fragment { "input_${name}":
     target  => "${conf_dir}/${conf_file}",
     order   => $order_input,
